ee5380985f55f9c2b0117990967ae9809d30b378
Prince Sharama <princesharmaofficial1@gmail.com>
2023-11-20 17:51:46 +0530

    first commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',  // Adjust the entry file as per your project structure
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
